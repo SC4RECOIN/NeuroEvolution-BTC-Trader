e353e61746be9d90c84fc42a7862bf943fb90ebf
Kurtis Streutker <kurtisstreutker@gmail.com>
2018-06-20 21:26:58 -0400

    updated

--- tf-evolution.py
+++ tf-evolution.py
@@ -3,72 +3,98 @@ import numpy as np
 from time import time
 import os
 
-from utils.population import Population
-from utils.network import Network
+from population.population import Population
+from population.network import Network
+from data.data_util import load_indicators
+import matplotlib.pyplot as plt
+from data.data_util import plot
 
 # suppress tf GPU logging
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+# interactive plots
+plt.ion()
 
-def calc_score(predictions, target):
-    # find model accuracy
-    correct = 0
-    for prediction, label in zip(predictions, target):
-        if np.argmax(prediction) == label: correct += 1
 
-    return correct / len(target) * 100
+def calculate_profit(trades, trade_prices):
+    btc_wallet = 0.
+    starting_cash = 100.
+    usd_wallet = starting_cash
+    fee = 0.001
+
+    # initially wait for buy signal
+    holding = False
+
+    for idx, trade in enumerate(trades):
+        if holding:
+            if not np.argmax(trade):
+                holding = False
+                usd_wallet = btc_wallet * trade_prices[idx] * (1 - fee)
+        else:
+            if np.argmax(trade):
+                holding = True
+                btc_wallet = usd_wallet / trade_prices[idx] * (1 - fee)
+
+    return (usd_wallet / starting_cash - 1) * 100
 
 
 if __name__ == '__main__':
-    # load mnist data and one-hot encode labels
-    mnist = {'test_images': np.load('data/test_images.npy'),
-             'test_labels': np.load('data/test_labels.npy'),
-             'train_images': np.load('data/train_images.npy'),
-             'train_labels': np.load('data/train_labels.npy')}
-
     # genetic parameters
-    pop_size = 50
-    mutation_rate = 0.05
+    pop_size = 150
+    w_mutation_rate = 0.05
+    b_mutation_rate = 0.
     mutation_scale = 0.3
-    breeding_ratio = 0.0
-    generations = 50
+    generations = 1000
 
     # network parameters
     network_params = {
-        'input' : 784,
-        'hidden': [256, 256],
-        'output': 10
+        'input': 4,
+        'hidden': [16, 32],
+        'output': 2
     }
 
     # build initial population
-    pop = Population(network_params, pop_size, mutation_rate, mutation_scale, breeding_ratio)
+    pop = Population(network_params, pop_size, mutation_scale, w_mutation_rate, b_mutation_rate)
     best_genome = pop.genomes[0]
+
+    # load indicators
+    indicators_train, indicators_test, prices_train, prices_test = load_indicators()
+
+    print('Buy and hold profit: {0:.2f}%'.format((prices_train[-1] / prices_train[0] - 1) * 100))
+    print('Train set size: ', indicators_train.shape[0], ' candles\n')
 
     # run for set number of generations
     for g in range(generations):
         start = time()
-        print('{}\ncreating generation {}...'.format('='*22, g + 1))
+        print('{}\ncreating generation {}...'.format('=' * 22, g + 1))
+
         pop.evolve()
 
         # open session and evaluate population
         with tf.Session() as sess:
             print('evaluating population...')
+
             for genome in pop.genomes:
-                output = sess.run(genome.model.prediction, feed_dict={genome.model.X: mnist['train_images']})
-                genome.score = calc_score(output, mnist['train_labels'])
+                actions = sess.run(genome.model.prediction, feed_dict={genome.model.X: indicators_train})
+
+                # profit score
+                genome.score = calculate_profit(actions, prices_train)
 
                 # save best genome
                 if genome.score > best_genome.score:
                     best_genome = genome
+                    genome.save()
 
-        print('average score: {0:.2f}'.format(np.average(np.array([x.score for x in pop.genomes]))))
-        print('best score: {0:.2f}'.format(max([x.score for x in pop.genomes])))
-        print('record score: {0:.2f}'.format(best_genome.score))
-        print('time: {0:.2f}s\n'.format(time() - start))
+            # plot best genome of generation
+            gen_best = pop.genomes[np.argmax(np.array([x.score for x in pop.genomes]))]
+            actions = sess.run(gen_best.model.prediction, feed_dict={gen_best.model.X: indicators_train})
+            actions_test = sess.run(gen_best.model.prediction, feed_dict={gen_best.model.X: indicators_test})
+            # plot(actions, prices_train, g)
+
+            print('average score: {0:.2f}'.format(np.average(np.array([x.score for x in pop.genomes]))))
+            print('best score: {0:.2f}'.format(max([x.score for x in pop.genomes])))
+            print('test score of best network: {0:.2f}%'.format(calculate_profit(actions_test, prices_test)))
+            print('record score: {0:.2f}'.format(best_genome.score))
+            print('time: {0:.2f}s\n'.format(time() - start))
 
         tf.reset_default_graph()
-
-    # test accuracy of best model
-    best_model = Network(best_genome)
-    output = tf.Session().run(best_model.prediction, feed_dict={best_model.X: mnist['test_images']})
-    print("\n\nFinished.\nTest accuracy of best model: {0:.2f}".format(calc_score(output, mnist['test_labels'])))
