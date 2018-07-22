cfbd11fbf4e284eebc35078864628ed713e1135d
Kurtis Streutker <kurtisstreutker@gmail.com>
2018-07-21 22:05:07 -0400

    added loading functionality

--- population/network.py
+++ population/network.py
@@ -1,32 +1,43 @@
 import tensorflow as tf
 import numpy as np
+import os
 
 
 class Network(object):
-    def __init__(self, params):
-
+    def __init__(self, params=None, load_path=None):
         self.X = None
-        self.Y = tf.placeholder("float", [None, params.outputs])
-        self.outputs = params.outputs
+        self.Y = None
+        self.weights, self.biases = [], []
+        self.prediction = None
 
         # only relevant for rnn
         self.timesteps = None
 
-        # apply weights and biases
-        self.weights, self.biases = [], []
-        for w, b in zip(params.weights, params.biases):
-            self.weights.append(tf.constant(w))
-            self.biases.append(tf.constant(b))
+        if params is None and load_path is None:
+            raise AttributeError('You must specify network parameters')
 
-        if params.network == 'feedforward':
+        # creating network from config params
+        if params is not None:
             self.X = tf.placeholder("float", [None, params.inputs])
+            self.Y = tf.placeholder("float", [None, params.outputs])
+
+            # apply weights and biases
+            for w, b in zip(params.weights, params.biases):
+                self.weights.append(tf.constant(w))
+                self.biases.append(tf.constant(b))
+
+            # network type
+            if params.network == 'feedforward':
+                self.prediction = self.feedforward()
+            elif params.network == 'recurrent':
+                raise AttributeError('recurrent networks not yet available')
+            else:
+                raise AttributeError(params.network, ' is not a valid network type')
+
+        # loading network from saved weights
+        if load_path is not None:
+            self.infer_network(load_path)
             self.prediction = self.feedforward()
-
-        elif params.network == 'recurrent':
-            raise AttributeError('recurrent networks not yet available')
-
-        else:
-            raise AttributeError(params.network, ' is not a valid network type')
 
     def feedforward(self):
         layer = self.X
@@ -37,3 +48,22 @@ class Network(object):
         layer = tf.add(tf.matmul(layer, self.weights[-1]), self.biases[-1])
 
         return tf.nn.softmax(layer)
+
+    def infer_network(self, load_dir):
+        layers = []
+        for file in os.listdir(load_dir):
+            path = load_dir + '/' + file
+            if 'weights{}'.format(len(self.weights)) in file:
+                weights = np.load(path)
+                layers.append(weights.shape[0])
+                if self.X is None:
+                    self.X = tf.placeholder("float", [None, weights.shape[0]])
+                    layers.pop(0)
+                self.weights.append(tf.constant(weights))
+                self.Y = tf.placeholder("float", [None, weights.shape[-1]])
+            elif 'biases{}'.format(len(self.biases)) in file:
+                self.biases.append(tf.constant(np.load(path)))
+            elif 'weights' in file or 'biases' in file:
+                raise Exception('files came in wrong order')
+
+        print('successfully loaded', layers, 'network')
