945a578f4e2588c3e9885166c1f6f241965faf75
Kurtis Streutker <kurtisstreutker@gmail.com>
2019-07-12 21:08:12 -0400

    renamed to import

--- tf_evolution.py
+++ tf_evolution.py
@@ -2,13 +2,9 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import random
-from sklearn.preprocessing import StandardScaler
-import joblib
 
 from population.population import Population
 from population.network import Network
-from server import send_data
-from finta import TA
 
 # suppress tf GPU logging
 import os
@@ -37,41 +33,6 @@ def calculate_profit(trades, prices):
     return (usd_wallet / starting_cash - 1) * 100
 
 
-def get_data(filepath, min_inv: int = 5):
-    ohlc = pd.read_csv(filepath, index_col=[0])
-    ohlc = ohlc.iloc[::min_inv, :]
-    print('Calculating TA...')
-
-    ta = [
-        TA.MOM(pd.DataFrame({'close': TA.ER(ohlc)}), 3),            # 0
-        TA.MOM(pd.DataFrame({'close': TA.PPO(ohlc)["HISTO"]}), 3),  # 1
-        TA.MOM(pd.DataFrame({'close': TA.STOCHRSI(ohlc)}), 3),      # 2
-        TA.MOM(pd.DataFrame({'close': TA.IFT_RSI(ohlc)}), 3),       # 3
-        TA.MOM(pd.DataFrame({'close': TA.ADX(ohlc)}), 3),           # 4
-        TA.MOM(pd.DataFrame({'close': TA.UO(ohlc)}), 3),            # 5
-        TA.MOM(pd.DataFrame({'close': TA.AO(ohlc)}), 3),            # 6
-        TA.MOM(pd.DataFrame({'close': TA.RSI(ohlc)}), 3),           # 7
-        TA.MOM(pd.DataFrame({'close': TA.COPP(ohlc)}), 3),          # 8
-        TA.MOM(pd.DataFrame({'close': TA.CCI(ohlc)}), 3),           # 9
-        TA.MOM(pd.DataFrame({'close': TA.CHAIKIN(ohlc)}), 3),       # 10
-        TA.MOM(pd.DataFrame({'close': TA.FISH(ohlc)}), 3)           # 11
-    ]
-    
-    # transpose and remove NaN 
-    ta = np.array(ta).transpose()
-    ta[np.isnan(ta)] = 0
-    ta[np.isinf(ta)] = 0
-    closing_prices = ohlc.values[:, ohlc.columns.get_loc('close')]
-
-    print('Scaling...')
-    scaler = StandardScaler()
-    scaler.fit(ta)
-    inputs = scaler.transform(ta)
-    joblib.dump(scaler, 'scaler.pkl')
-
-    return inputs, closing_prices
-
-
 def get_rand_segment(inputs, prices, size):
     max_idx = len(prices) - size
     rand_idx = np.random.randint(0, max_idx)
@@ -87,9 +48,7 @@ def get_rand_col(inputs, col):
     return np.stack(cols, axis=1), idxs
 
 
-def run_model():
-    inputs, prices = get_data('data/coinbase-1min.csv', 15)
-    
+def train_model(inputs, prices):
     # genetic parameters
     pop_size = 200
     w_mutation_rate = 0.05
