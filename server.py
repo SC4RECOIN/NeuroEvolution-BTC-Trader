1af3810f33266f0c515f7aaec8c63cabc238658d
Kurtis Streutker <kurtisstreutker@gmail.com>
2019-07-18 21:27:02 -0400

    serving static files

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from threading import Thread
@@ -8,13 +8,14 @@ from tf_evolution import train_model
 from finta import TA
 import pandas as pd
 import numpy as np
+import os
 import json
 
 import logging
 log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='interface/build')
 CORS(app)
 socketio = SocketIO(app)
 
@@ -49,9 +50,22 @@ def start_training():
     def reporter(name, data):
         socketio.emit(name, json.dumps(data), broadcast=True)
 
-    args = (hidden_layers, data.ta, data.ohlc, reporter,)
+    args = (hidden_layers, data.ta, data.get_closing(), reporter,)
     Thread(target=train_model, args=args).run()
     return jsonify({"message": "training started"})
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def app_page(path):
+    # check if react build files exist
+    if not os.path.isdir("interface/build"):
+        return "App is running (dev/cli)"
+    
+    if path != "" and os.path.exists(f"interface/build/{path}"):
+        return send_file(f"interface/build/{path}")
+
+    return send_file("interface/build/index.html")
 
 
 if __name__ == "__main__":
